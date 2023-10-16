0584cb4ef40147ad0cafe2add14cc9ddb99fd8ce
Artem0131 <145832591+Artem0131@users.noreply.github.com>
2023-10-16 18:23:07 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL = "https://4e503f5a-694f-4d6e-bb13-247ddfcaa14e.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+ORDER_TRACK_INFO = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Biba",
+    "lastName": "Boba",
+    "address": "Red square 1",
+    "metroStation": 5,
+    "phone": "+7 909 999 90 06",
+    "rentTime": 1,
+    "deliveryDate": "2023-07-13",
+    "comment": "Biba i Boba",
+    "color": [
+        "BLACK"
+    ]
+}
